b1a84c38a1f7765e227de40bd37085cd4715bef1
lmerat46 <merat.laure@hotmail.fr>
2018-12-07 18:29:49 +0100

    RecyclerView Clickable

--- app/src/main/java/com/androidclass/stryker/mplrss/FluxAdapter.java
+++ app/src/main/java/com/androidclass/stryker/mplrss/FluxAdapter.java
@@ -1,6 +1,10 @@
 package com.androidclass.stryker.mplrss;
 
-import android.support.annotation.NonNull;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,12 +16,59 @@ import java.util.List;
 public class FluxAdapter extends RecyclerView.Adapter<FluxAdapter.FeedModelViewHolder> {
 
     List<Flux> l;
-    public static class FeedModelViewHolder extends RecyclerView.ViewHolder {
+    public static class FeedModelViewHolder extends RecyclerView.ViewHolder implements ListeRSS.OnFragmentInteractionListener, ListeRSSFav.OnFragmentInteractionListener, ListeRSSSearch.OnFragmentInteractionListener {
         private View fluxView;
+        private FragmentManager f;
 
         public FeedModelViewHolder(View v) {
             super(v);
             fluxView = v;
+            fluxView.setClickable(true);
+            fluxView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // TODO : lancer Fragment avec liste de RSS
+                    Intent i = new Intent(fluxView.getContext(), MainActivity.class);
+                    fluxView.getContext().startActivity(i);
+                    f = ((FragmentActivity) fluxView.getContext()).getSupportFragmentManager();
+                    FragmentTransaction t = f.beginTransaction();
+                    ListeRSS p = ListeRSS.newInstance();
+                    p.setContentResolver(fluxView.getContext());
+                    //TODO : continuer pour lancer Fragment
+                    t.add(R.id.liste_fragment_frame, p);
+                    t.commit();
+                    System.out.println("TEST::::::::::::::::::::::::");
+                }
+            });
+        }
+
+        @Override
+        public void onFragmentInteraction(Uri uri) {
+
+        }
+
+        @Override
+        public void onRSSSelection(String title) {
+            UnRSS u = UnRSS.newInstance(title, false);
+            u.setContentResolver(fluxView.getContext());
+            FragmentTransaction t = f.beginTransaction();
+            //t.replace(R.id.liste_fragment_frame, ListPaysFragment.newInstance());
+            t.replace(R.id.liste_fragment_frame, u);
+            t.addToBackStack(null);
+            //t.addToBackStack(null);FragmentTransaction t;
+            t.commit();
+        }
+
+        @Override
+        public void onRSSSelectionFav(String title) {
+            UnRSS u = UnRSS.newInstance(title, true);
+            u.setContentResolver(fluxView.getContext());
+            FragmentTransaction t = f.beginTransaction();
+            //t.replace(R.id.liste_fragment_frame, ListPaysFragment.newInstance());
+            t.replace(R.id.liste_fragment_frame, u);
+            t.addToBackStack(null);
+            //t.addToBackStack(null);FragmentTransaction t;
+            t.commit();
         }
     }
 
