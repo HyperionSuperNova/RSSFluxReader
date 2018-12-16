f88e087c9fb51d149a815400f2b9d035dbc5dccc
WinStryke <n.amari.pro@gmail.com>
2018-12-16 17:57:41 +0100

    custom webview + redesign

---
+++ app/src/main/java/com/androidclass/stryker/mplrss/MyWebView.java
@@ -0,0 +1,27 @@
+package com.androidclass.stryker.mplrss;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.webkit.WebView;
+
+public class MyWebView extends WebView {
+
+    public MyWebView(Context context) {
+        super(context);
+    }
+
+    public MyWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyWebView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        requestDisallowInterceptTouchEvent(true);
+        return super.onTouchEvent(event);
+    }
+}
--- app/src/main/java/com/androidclass/stryker/mplrss/UnRSS.java
+++ app/src/main/java/com/androidclass/stryker/mplrss/UnRSS.java
@@ -12,6 +12,8 @@ import android.support.v4.content.CursorLoader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -38,6 +40,7 @@ public class UnRSS extends Fragment {
     private TextView title;
     private TextView link;
     private TextView description;
+    private WebView wv;
 
     public UnRSS() {
     }
@@ -68,7 +71,6 @@ public class UnRSS extends Fragment {
         if (getArguments() != null) {
             titleRSS= getArguments().getString(ARG_PARAM1);
             favRSS = getArguments().getBoolean(ARG_PARAM2);
-
         }
     }
 
@@ -79,8 +81,9 @@ public class UnRSS extends Fragment {
         title = (TextView) v.findViewById(R.id.title);
         link = (TextView) v.findViewById(R.id.link);
         description = (TextView) v.findViewById(R.id.description);
-
-
+        wv = v.findViewById(R.id.Webview);
+        wv.setWebViewClient(new WebViewClient());
+        wv.setVerticalScrollBarEnabled(true);
         Uri.Builder builder = new Uri.Builder();
         builder.scheme("content").authority(authority).appendPath("rss");
         Uri uri = builder.build();
@@ -88,15 +91,15 @@ public class UnRSS extends Fragment {
         Cursor cc = c.loadInBackground();
         cc.moveToFirst();
 
-        title.setText(cc.getString(cc.getColumnIndexOrThrow("title")));
-        link.setText(cc.getString(cc.getColumnIndexOrThrow("link")));
+        title.setText(cc.getString(cc.getColumnIndexOrThrow("title")).trim());
+        link.setText(cc.getString(cc.getColumnIndexOrThrow("link")).trim());
         description.setText(cc.getString(cc.getColumnIndexOrThrow("description")));
 
 
         Button maListe = (Button) v.findViewById(R.id.maListe);
         Button sup = (Button) v.findViewById(R.id.supprimer);
         Button open = (Button) v.findViewById(R.id.gotoBrowser);
-
+        Button launchwebview = v.findViewById(R.id.openInWebView);
         if(favRSS){
             maListe.setVisibility(View.INVISIBLE);
             sup.setVisibility(View.VISIBLE);
@@ -141,6 +144,13 @@ public class UnRSS extends Fragment {
                 c.put("choisi", 0);
                 contentResolver.update(uri, c, "title = ?", new String [] {title.getText().toString()});
                 getFragmentManager().popBackStack();
+            }
+        });
+
+        launchwebview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wv.loadUrl(link.getText().toString());
             }
         });
 
